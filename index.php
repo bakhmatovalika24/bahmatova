1514d0169f091d00e0253fd37535400faae37cdc
Бахматова Анжелика <blkaede@gmail.com>
2025-04-15 11:43:16 +0300

    Добавлен favicon и head.php

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <title>Главная</title>
     <link rel="stylesheet" href="/assets/css/style.css">
+<link rel="icon" type="image/x-icon" href="/assets/images/favicon.ico">
+
 </head>
 <body>
     <?php include 'includes/header.php'; ?>
